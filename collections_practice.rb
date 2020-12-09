f24fafa13af5e78d4a176877bd09e214a4cfa24a
Alex Cline <rev.a.r.cline@gmail.com>
2020-12-09 11:44:03 -0600

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(array)
+  array.sort { |a, b| a <=> b }
+end
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[2], array[1] = array[1], array[2]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |name| name[2] = '$' }
+end
+
+def find_a(array)
+  array.select { |item| item if item[0].downcase == 'a' }
+end
+
+def sum_array(array)
+  sum = 0
+  array.each { |item| sum += item }
+  sum
+end
+
+def add_s(array)
+  array.each_with_index.collect do |item, index|
+    if index == 1
+      item
+    else
+      item << 's'
+    end
+  end
+end
